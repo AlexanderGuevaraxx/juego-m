a2d1a2b308f12385519e5b239b5941a72c54b266
anganevagg <42720094+anganevagg@users.noreply.github.com>
2018-10-20 10:26:45 -0500

    Add files via upload

--- motor.js
+++ motor.js
@@ -1,7 +1,26 @@
 //w es largo
 //h es ancho
+
 class sprite{
 	constructor(x,y,dir,w,h){
+		var im=new Image();
+		this.x=x;
+		this.y=y;
+		this.dir=im.src
+	}
+	draw(){
 
 	}
 }
+
+class game{
+	constructor(){
+
+	}
+	main(){
+		const canvas=document.getElementById('canvas');
+		var ctx=canvas.getContext("2d");
+	}
+}
+
+window.addEventListener("load",game.main,false);
