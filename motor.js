db647fa169df43524f36b6ffbdf49f83de3dceb7
anganevagg <42720094+anganevagg@users.noreply.github.com>
2018-10-20 10:04:57 -0500

    Add files via upload

--- motor.js
+++ motor.js
@@ -1,14 +1,1 @@
-var config = {
-    width: 800,
-    height: 600,
-    renderer: Phaser.AUTO,
-    antialias: true,
-    multiTexture: true,
-    state: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-}
-
-var game = new Phaser.Game(config);
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'canvas', { preload: preload, create: create });
